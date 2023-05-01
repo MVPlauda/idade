f9376a28a29693a0fd670192fe83b102a79bbf90
MVPlauda <plaudadev@gmail.com>
2023-05-01 18:56:10 -0300

    idade

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int idade;
+
+        Console.WriteLine("Qual a sua idade?: ");
+        idade = int.Parse(Console.ReadLine());
+
+        if (idade <= 12)
+        {
+            Console.WriteLine("De acordo com sua idade você ainda é uma criança. ");
+        }
+
+        else if (idade >= 18)
+        {
+            Console.WriteLine("De acordo com sua idade você é um adulto. ");
+        }
+
+        else
+        {
+
+            Console.WriteLine("De acordo com sua idade você é um adolescente. ");
+        }
+    }
+}
